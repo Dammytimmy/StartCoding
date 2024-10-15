7d79b8b797fb86bc58ab309ae4b7e7ad98bda70b
Dammytimmy <160670308+Dammytimmy@users.noreply.github.com>
2024-10-15 20:45:32 +0100

    fixed the bug in the troubleshooting js

---
+++ UnderstandingJavascript/troubleshooting.js
@@ -0,0 +1,15 @@
+function troubleshooting()
+{
+    const a = 1;
+    const b = 1;
+
+    let result;
+
+    result = a + b;
+    console.log(result);
+    
+
+    return result;
+}
+
+alert(troubleshooting);
